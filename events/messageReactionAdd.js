6a4aecbef6ca3049df8c5b213130e776238b782c
Cody Doerr (Pian) <cody.doerr@yahoo.com>
2023-03-17 15:03:29 -0500

    Update messageReactionAdd.js
    added test line to pull all channels in the channel id object in the emojiRoles json

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -11,10 +11,13 @@ module.exports = {
 		}
 		if (user != reaction.client.user) {
 			if (emojiJSON.hasOwnProperty(reaction.emoji.name) &&
-				(reaction.message.channelId == '1081301966510100480' ||
-					reaction.message.channelId == '1081355333357817866'||
-					reaction.message.channelId == '1083775765474189368'||
-					reaction.message.channelId == '1073263946083082301')
+			(reaction.message.channelId == '1081301966510100480' ||
+				reaction.message.channelId == '1081355333357817866'||
+				reaction.message.channelId == '1083775765474189368'||
+				reaction.message.channelId == '1073263946083082301'||
+				reaction.message.channelId == '890793150233317436'||
+				reaction.message.channelId == '1086296730137542677')
+					/*emojiJSON.categories.clubGameChannel.some(element => element === reaction.message.channelId)*/
 			) {
 				TestRole = reaction.message.guild.roles.cache.find((role) => role.name === emojiJSON[reaction.emoji.name].role.name);
 				reaction.message.guild.members.cache.find(member => member.id === user.id).roles.add(TestRole);
