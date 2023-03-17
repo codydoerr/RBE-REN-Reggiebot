7db638fba4daf5d3ce4f8566bbcff343f9ed8c61
Cody Doerr (Pian) <cody.doerr@yahoo.com>
2023-03-17 15:03:37 -0500

    Update messageReactionRemove.js
    added overwatch

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -14,7 +14,9 @@ module.exports = {
 				(reaction.message.channelId == '1081301966510100480' ||
 					reaction.message.channelId == '1081355333357817866'||
 					reaction.message.channelId == '1083775765474189368'||
-					reaction.message.channelId == '1073263946083082301')
+					reaction.message.channelId == '1073263946083082301'||
+					reaction.message.channelId == '890793150233317436'||
+					reaction.message.channelId == '1086296730137542677')
 			) {
 				TestRole = reaction.message.guild.roles.cache.find((role) => role.name === emojiJSON[reaction.emoji.name].role.name);
 				reaction.message.guild.members.cache.find(member => member.id === user.id).roles.remove(TestRole);
