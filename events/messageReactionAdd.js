c4a0366fbe8566ab271abe8db183406920a34f7f
Cody Doerr (Pian) <cody.doerr@yahoo.com>
2023-03-17 09:19:28 -0500

    Update messageReactionAdd.js
    Removed modals from messageReactionAdd

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,9 +1,7 @@
 const emojiJSON = require('../emojiRoles.json')
-const { Modal } = require('discord.js');
 module.exports = {
 	name: 'messageReactionAdd',
 	async execute(reaction, user) {
-		let modal = new Modal();
 		if (reaction.partial) {
 			try {
 				await reaction.fetch();
@@ -20,12 +18,6 @@ module.exports = {
 			) {
 				TestRole = reaction.message.guild.roles.cache.find((role) => role.name === emojiJSON[reaction.emoji.name].role.name);
 				reaction.message.guild.members.cache.find(member => member.id === user.id).roles.add(TestRole);
-				if(reaction.message.channelId == '1073263946083082301' && emojiJSON.categories.clubGames.some(name => reaction.emoji.name === name)){
-					modal = new Modal()
-						.setCustomId('myModal')
-						.setTitle('My Modal');
-					await reaction.showModal(modal);
-				}
 			}
 			
 		}
